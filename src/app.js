b16b394b420f00b7b577efd327da59acfbbfe4e8
Damon <damondias@gmail.com>
2023-08-04 18:48:23 -0400

    feat: Configure server Express and Database Postgress

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express"
+import cors  from "cors"
+import dotenv from "dotenv"
+import router from "./routes/index.routes.js";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express());
+app.use(router);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`
+    Running API Shortly on port ${PORT},
+    Url: http://localhost:${PORT}
+`));
