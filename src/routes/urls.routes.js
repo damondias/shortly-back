5b8b6e061e5013f6e3091b439e9bb68675b2032e
Damon <damondias@gmail.com>
2023-08-07 10:56:23 -0400

    feat: shorteURL correct

--- src/routes/urls.routes.js
+++ src/routes/urls.routes.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { validateToken } from "../middlewares/validateToken.js";
+import { validateSchema } from "../middlewares/validateSchema.js";
+import { urlSchema } from "../schemas/urls.schemas.js";
+import { postShorten } from "../controllers/urls.controllers.js";
 
 const urlsRouter = Router();
 
 
 urlsRouter.use(validateToken);
 
-urlsRouter.post("/urls/shorten", )
+urlsRouter.post("/urls/shorten", validateSchema(urlSchema), postShorten)
 
 export default urlsRouter;
