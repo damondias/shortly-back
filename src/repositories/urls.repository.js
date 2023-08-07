41715b6e732eb90258302a94e07ac82251758a53
Damon <damondias@gmail.com>
2023-08-07 12:14:48 -0400

    feat: GET and Delete route of open shortenURL or delete url

--- src/repositories/urls.repository.js
+++ src/repositories/urls.repository.js
@@ -3,7 +3,7 @@ import { db } from "../database/database.connection.js";
 function createShorten(url, shortUrl, id) {
     return db.query(`
         INSERT INTO urls(url, "shortUrl", "userId")
-        VALUES ($1, $2, $3)
+        VALUES ($1, $2, $3) ;
     `, [url, shortUrl, id])
 }
 
@@ -11,13 +11,40 @@ function findUrlById(id) {
     return db.query(`
         SELECT id, "shortUrl", url 
             FROM urls 
-            WHERE id = $1
+            WHERE id = $1 ;
     `, [id]);
+}
+
+function findUrlByShorten(shortUrl) {
+    return db.query(`
+        SELECT * 
+            FROM urls 
+            WHERE "shortUrl" = $1 ;
+    `,[shortUrl]);
+}
+
+function incrementUrlVisitCount(id){
+    return db.query(`
+        UPDATE urls
+            SET "visitCount" = "visitCount" + 1
+            WHERE id = $1
+    `,[id]);
+}
+
+function deleteUrl(id) {
+    return db.query(`
+        DELETE 
+            FROM urls 
+            WHERE id=$1
+    `, [id])
 }
 
 const urlsRepository ={
     createShorten,
     findUrlById,
+    findUrlByShorten,
+    incrementUrlVisitCount,
+    deleteUrl
 }
 
 export default urlsRepository
