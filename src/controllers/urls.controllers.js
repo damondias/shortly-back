fed352cd04d534fef93317591264b4eed5128c16
Damon <damondias@gmail.com>
2023-08-07 12:49:46 -0400

    fix: add id correct in sendStatus 201

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -8,7 +8,7 @@ export async function postShorten(req,res) {
     try {
         const shortUrl = nanoid(8);
         await urlsRepository.createShorten(url, shortUrl, id);
-        res.status(201).send({ shortUrl }); 
+        res.status(201).send({ id, shortUrl }); 
 
     } catch (error) {
         res.status(500).send(error.message);
